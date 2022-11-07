cb8e1f07ca3355b07c35a8c7d062d158f372d5e9
sintahardianti <115516473+sintahardianti@users.noreply.github.com>
2022-11-07 18:29:50 +0700

    Create lab2-latihan1.py

---
+++ lab2-latihan1.py
@@ -0,0 +1,8 @@
+a = int(input("masukkan angka pertama: "))
+b = int(input("masukkan angka kedua: "))
+print()
+if a>b:
+    print (a, "adalah angka terbesar")
+else:
+    b>a
+    print ("angka terbesar: ",b ) 
